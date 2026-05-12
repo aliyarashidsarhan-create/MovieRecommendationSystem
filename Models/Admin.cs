d20d440b9645fd18e9462b004b477f675ca27e57
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-05-12 14:30:49 +0400

    adding Models/Admin.cs

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace MovieRecommendationSystem.Models
 {
-    internal class Admin
+    // Admin also inherits from Person.
+    public class Admin : Person
     {
+        public Admin(int id, string username, string password)
+            : base(id, username, password)
+        {
+        }
     }
+
 }

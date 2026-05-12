583f28f0b5f5f301b1a437c61aab2fb0fef6f774
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-05-12 14:26:44 +0400

    Models/Person.cs

--- Models/Person.cs
+++ Models/Person.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace MovieRecommendationSystem.Models
 {
-    internal class Person
+    // Abstract class because Person is a general idea.
+    // User and Admin will inherit from it.
+    public abstract class Person
     {
+        // Common properties for all people in the system.
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        // Constructor to initialize common data.
+        public Person(int id, string username, string password)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+        }
     }
 }

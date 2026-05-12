9dcc08ded7481a5bb6c638db43e79e88c62660ac
“Anoud <anoudalsaidi1993@gmail.com>
2026-05-12 16:56:29 +0400

    Adding Services/RecommendationService.cs

--- Services/RecommendationService.cs
+++ Services/RecommendationService.cs
@@ -1,10 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MovieRecommendationSystem.Interfaces;
+using MovieRecommendationSystem.Models;
 
 namespace MovieRecommendationSystem.Services
 {
-    internal class RecommendationService
+    public class RecommendationService
     {
+        private List<IRecommendation> _strategies;
+
+        public RecommendationService()
+        {
+            // Polymorphism: same interface, different recommendation strategies.
+            _strategies = new List<IRecommendation>
+            {
+                new ContentBasedService(),
+                new CollaborativeFilteringService()
+            };
+        }
+
+        public List<Movie> GetRecommendations(User user, List<Movie> movies, List<Rating> ratings, List<User> users)
+        {
+            List<Movie> finalRecommendations = new List<Movie>();
+
+            foreach (var strategy in _strategies)
+            {
+                var result = strategy.Recommend(user, movies, ratings, users);
+                finalRecommendations.AddRange(result);
+            }
+
+            // Remove duplicate movies and sort by movie rating.
+            return finalRecommendations
+                .GroupBy(m => m.Id)
+                .Select(g => g.First())
+                .OrderByDescending(m => m.Rating)
+                .Take(5)
+                .ToList();
+        }
     }
 }

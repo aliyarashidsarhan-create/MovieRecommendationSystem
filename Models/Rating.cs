3f82b5be934388e6b4e5705b112cfa2c60280e7f
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-05-12 14:36:14 +0400

    add Models/Rating.cs

--- Models/Rating.cs
+++ Models/Rating.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace MovieRecommendationSystem.Models
 {
-    internal class Rating
+    public class Rating
     {
+        // ID of the user who rated the movie.
+        public int UserId { get; set; }
+
+        // ID of the movie being rated.
+        public int MovieId { get; set; }
+
+        // Rating value from 1 to 5.
+        public int Score { get; set; }
     }
 }

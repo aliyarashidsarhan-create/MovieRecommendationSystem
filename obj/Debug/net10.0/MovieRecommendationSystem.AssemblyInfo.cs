8b70367b55e3ad492d16b3fbeb60dbc59dfe987a
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-05-18 10:21:48 +0400

    Final UI and project improvements

--- obj/Debug/net10.0/MovieRecommendationSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/MovieRecommendationSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieRecommendationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c168de486c1f84070a9a3370a1a2bbbe46e5615")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18c40ebdfea43e40934332f1d988748979f51af9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieRecommendationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieRecommendationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
